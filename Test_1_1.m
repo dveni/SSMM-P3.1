7465419a093f466342d737fd00e7e420ac8ad6da
Daveni <daveni@protonmail.com>
2020-03-13 17:59:18 +0100

    Test_1_1 terminado

--- Test_1_1.m
+++ Test_1_1.m
@@ -1,9 +1,21 @@
 barrasRGB = imread('barrasRGB.bmp');
-subplot(2,3,2);
+
+R = barrasRGB(:,:,1);
+G = barrasRGB(:,:,2);
+B = barrasRGB(:,:,3);
+
+subplot(3,3,2);
 imshow(barrasRGB);
-subplot(2,3,4);
-imshow(barrasRGB(:,:,1));
-subplot(2,3,5);
-imshow(barrasRGB(:,:,2));
-subplot(2,3,6);
-imshow(barrasRGB(:,:,3));
+subplot(3,3,4);
+imshow(R);
+subplot(3,3,5);
+imshow(G);
+subplot(3,3,6);
+imshow(B);
+
+imageGBR(:,:,1) = G;
+imageGBR(:,:,2) = B;
+imageGBR(:,:,3) = R;
+
+subplot(3,3,8);
+imshow(imageGBR);
